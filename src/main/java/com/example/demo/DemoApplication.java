b64878ad60c853c5f9eaac0a12b9d2e165c0d9fc
Shadman Ali <shadman.ali@live.co.uk>
2024-10-04 10:47:07 +0530

    use CommandLineRunner

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,17 +1,32 @@
 package com.example.demo;
 
+import com.example.demo.run.Location;
+import com.example.demo.run.Run;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
 
 @SpringBootApplication
 public class DemoApplication {
 
 	private static final Logger log = LoggerFactory.getLogger(DemoApplication.class);
 	public static void main(String[] args) {
-		ConfigurableApplicationContext context = SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(DemoApplication.class, args);
 		log.info("Something changed");
+	}
+
+	@Bean
+	CommandLineRunner runner() {
+		return args -> {
+			Run run = new Run(1, "First Run", LocalDateTime.now(), LocalDateTime.now().plus(1, ChronoUnit.HOURS) , 5, Location.OUTDOOR);
+			log.info("Run : "+ run);
+		};
 	}
 }
---
+++ src/main/java/com/example/demo/run/Location.java
@@ -0,0 +1,6 @@
+package com.example.demo.run;
+
+public enum Location {
+    INDOOR,
+    OUTDOOR
+}
