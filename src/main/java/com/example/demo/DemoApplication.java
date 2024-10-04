a0b8e5a2e6868dfaec4903bf67de492766531910
Shadman Ali <shadman.ali@live.co.uk>
2024-10-04 09:55:32 +0530

    [MOD] update

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,13 +1,13 @@
-package ali.shadman.project;
+package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ProjectApplication {
+public class DemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ProjectApplication.class, args);
+		SpringApplication.run(DemoApplication.class, args);
 	}
 
 }
